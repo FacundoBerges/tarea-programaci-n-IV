73b1721bf1107585b1a913dae850e11acfa5920e
Facundo Berges <facundo.h.berges@gmail.com>
2025-04-10 15:30:26 -0300

    cambios en estilos y paquetes npm, cambios en listado de auto

--- src/app/components/listado-auto/listado-auto.component.ts
+++ src/app/components/listado-auto/listado-auto.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  input,
+  InputSignal,
+  OnInit,
+  output,
+  OutputEmitterRef,
+} from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+import { Auto } from '../../interfaces/auto';
 
 @Component({
   selector: 'app-listado-auto',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './listado-auto.component.html',
-  styleUrl: './listado-auto.component.scss'
+  styleUrl: './listado-auto.component.scss',
 })
-export class ListadoAutoComponent {
+export class ListadoAutoComponent implements OnInit {
+  public autoEmmiterRef: OutputEmitterRef<Auto> = output<Auto>();
+  public autoSeleccionado?: Auto;
+  public autos: InputSignal<Auto[]> = input<Auto[]>([]);
 
+  ngOnInit(): void {
+    if (!this.autos) {
+      throw new Error('Autos input is required');
+    }
+  }
+
+  public seleccionarAuto(auto: Auto): void {
+    this.autoSeleccionado = auto;
+    this.autoEmmiterRef.emit(this.autoSeleccionado);
+  }
 }
